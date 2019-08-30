fe7851dbc6863943861f293de7f3b79d8bdd9573
Sebastian <Dumox@someplace.com>
2019-08-30 22:21:22 +0200

    Changed formating

--- js/WeatherClass.js
+++ js/WeatherClass.js
@@ -1,35 +1,35 @@
-class WeatherClass{
-    constructor(weatherConditions){
-        this.city =  weatherConditions.name,
-        this.humidity = Math.round(weatherConditions.main.humidity),
-        this.pressure = Math.round(weatherConditions.main.pressure),
-        this.temp = Math.round(weatherConditions.main.temp),
-        this.tempMax = Math.round(weatherConditions.main.temp_max),
-        this.tempMin = Math.round(weatherConditions.main.temp_min)
+class WeatherClass {
+    constructor(weatherConditions) {
+        this.city = weatherConditions.name,
+            this.humidity = Math.round(weatherConditions.main.humidity),
+            this.pressure = Math.round(weatherConditions.main.pressure),
+            this.temp = Math.round(weatherConditions.main.temp),
+            this.tempMax = Math.round(weatherConditions.main.temp_max),
+            this.tempMin = Math.round(weatherConditions.main.temp_min)
         this.description = weatherConditions.weather[0].description,
-        this.windSpeed = Math.round(weatherConditions.wind.speed),
-        this.icon = weatherConditions.weather[0].icon
+            this.windSpeed = Math.round(weatherConditions.wind.speed),
+            this.icon = weatherConditions.weather[0].icon
     }
 
-    calculateWindSpeed(){       
+    calculateWindSpeed() {
         return this.windSpeed * 3.6;
     }
 
-    calculateDetectableTemp(){
+    calculateDetectableTemp() {
         const windSpeed = this.calculateWindSpeed();
-        return  Math.round(13.12 + (0.6215 * this.temp) - Math.pow(11.37*windSpeed, 0.16) 
-                + Math.pow(0.3965 * windSpeed * this.temp, 0.16));
+        return Math.round(13.12 + (0.6215 * this.temp) - Math.pow(11.37 * windSpeed, 0.16)
+            + Math.pow(0.3965 * windSpeed * this.temp, 0.16));
     }
 
-    savelastSearchedLocationInLocalStorage(inputValue){
-        try{
+    savelastSearchedLocationInLocalStorage(inputValue) {
+        try {
             localStorage.setItem("weatherConditionsLastLocation", JSON.stringify(inputValue));
-        }catch(error){
+        } catch (error) {
             Error(error);
         }
     }
 
-    showCurrentWeatherConditions(){
+    showCurrentWeatherConditions() {
         const iconAPiURL = "http://openweathermap.org/img/wn/";
         const weatherCurrentIcone = document.querySelector(".current-weather-img");
         const weatherCurrentDescription = document.querySelector(".current-weather-describe");
@@ -40,7 +40,7 @@ class WeatherClass{
         const weatherConditionsDetailsHumidity = document.querySelector(".humidity-value");
         const weatherConditionsDetailsPressure = document.querySelector(".pressure-value");
         const weatherCurrentDetailsWindSpeed = document.querySelector(".wind-value");
-        const weatherCurrentDetectableTemp =  document.querySelector(".detectable-value");
+        const weatherCurrentDetectableTemp = document.querySelector(".detectable-value");
 
 
         weatherCurrentIcone.setAttribute("src", iconAPiURL + this.icon + "@2x.png");
@@ -48,7 +48,7 @@ class WeatherClass{
         weatherCurrentDescription.textContent = this.description;
         weatherCurrentTemp.textContent = this.temp + " \u2103";
         weatherCurrentCity.textContent = this.city;
-        weatherCurrentDetailsTempMax.textContent  = this.tempMax + " \u2103"; 
+        weatherCurrentDetailsTempMax.textContent = this.tempMax + " \u2103";
         weatherCurrentDetailsTempMin.textContent = this.tempMin + " \u2103";
         weatherConditionsDetailsHumidity.textContent = this.humidity + " %";
         weatherConditionsDetailsPressure.textContent = this.pressure + " hPa";
@@ -56,14 +56,14 @@ class WeatherClass{
         weatherCurrentDetectableTemp.textContent = this.calculateDetectableTemp() + " \u2103";
     }
 
-    static async downloadCurrentWeatherConditions(params = {q: "warszawa"}){ 
+    static async downloadCurrentWeatherConditions(params = { q: "warszawa" }) {
         console.log(params);
-        return await axios.get("http://api.openweathermap.org/data/2.5/weather?appid=92c581f751a8ab015bc220ef2217adf5&units=metric&lang=pl&", 
-                                {params});
+        return await axios.get("http://api.openweathermap.org/data/2.5/weather?appid=92c581f751a8ab015bc220ef2217adf5&units=metric&lang=pl&",
+            { params });
     }
 }
 
-document.querySelector(".activate-details-btn").addEventListener("click", ()=>{
+document.querySelector(".activate-details-btn").addEventListener("click", () => {
     const weatherCnt = document.querySelector(".current-results-cnt");
     weatherCnt.classList.toggle("active-details");
 })
--- js/main.js
+++ js/main.js
@@ -2,43 +2,43 @@ import WeatherClass from "./WeatherClass.js"
 import AirQuality from "./AirQualityClass.js"
 
 // Gelokalizacja
-navigator.geolocation.getCurrentPosition((position) =>{
+navigator.geolocation.getCurrentPosition((position) => {
     console.log(position.coords);
-     const parameters = {
+    const parameters = {
         lat: position.coords.latitude,
         lon: position.coords.longitude
     }
-    WeatherClass.downloadCurrentWeatherConditions(parameters).then(resolve =>{
+    WeatherClass.downloadCurrentWeatherConditions(parameters).then(resolve => {
         const currentWeaherConditions = new WeatherClass(resolve.data);
         currentWeaherConditions.showCurrentWeatherConditions();
         AirQuality.downloadtAirQualityData(position.coords.latitude, position.coords.longitude).then(resolve => {
             const airQualityResults = new AirQuality(resolve.data);
             document.querySelector(".current-air-results").setAttribute("style", "display: grid");
             airQualityResults.showAirQuality();
-        }).catch(error => {Error(error)})
+        }).catch(error => { Error(error) })
     })
-    .catch((error => Error(error)))
+        .catch((error => Error(error)))
 
 }, reject => {
     //Nie udzielenie zgody na geolokaizacje
     //wywoływanie pogody dla ostatniej lokalizacji
-    if(localStorage.getItem("weatherConditionsLastLocation") != ""){
-        const lastSearchedLocalization =  JSON.parse(localStorage.getItem("weatherConditionsLastLocation"));
+    if (localStorage.getItem("weatherConditionsLastLocation") != "") {
+        const lastSearchedLocalization = JSON.parse(localStorage.getItem("weatherConditionsLastLocation"));
         WeatherClass.downloadCurrentWeatherConditions(lastSearchedLocalization).then(resolve => {
             const currentWeatherConditions = new WeatherClass(resolve.data);
             currentWeatherConditions.showCurrentWeatherConditions();
             const lastSearchedLocalizationGeoCords = JSON.parse(localStorage.getItem("airQualityGeoCords"));
-            if(localStorage.getItem("airQualityFlag") === "false"){
+            if (localStorage.getItem("airQualityFlag") === "false") {
                 document.querySelector(".current-air-results").setAttribute("style", "display: grid");
-                AirQuality.downloadtAirQualityData(lastSearchedLocalizationGeoCords.lat, lastSearchedLocalizationGeoCords.lng).then(resolve =>{
+                AirQuality.downloadtAirQualityData(lastSearchedLocalizationGeoCords.lat, lastSearchedLocalizationGeoCords.lng).then(resolve => {
                     const airQualityResults = new AirQuality(resolve.data)
                     airQualityResults.showAirQuality();
-                }) 
-            }else{
+                })
+            } else {
                 document.querySelector(".current-air-results").setAttribute("style", "display: none");
-            }                        
+            }
         })
-    }else{
+    } else {
         WeatherClass.downloadCurrentWeatherConditions().then(resolve => {
             const currentWeatherClass = new WeatherClass(resolve.data);
             console.log(resolve.data);
@@ -50,18 +50,18 @@ navigator.geolocation.getCurrentPosition((position) =>{
             })
         })
     }
-        
+
 });
- 
+
 //Obsługa wyszukiwania przez submit
 
-document.querySelector(".input-text-submit").addEventListener("click", ()=>{
+document.querySelector(".input-text-submit").addEventListener("click", () => {
     event.preventDefault();
     const parameters = {
         q: document.querySelector(".input-text").value
     }
-    document.querySelector(".input-text").value =  "";
-    WeatherClass.downloadCurrentWeatherConditions(parameters).then(resolve =>{
+    document.querySelector(".input-text").value = "";
+    WeatherClass.downloadCurrentWeatherConditions(parameters).then(resolve => {
         const currentWeaherConditions = new WeatherClass(resolve.data);
         currentWeaherConditions.showCurrentWeatherConditions();
         currentWeaherConditions.savelastSearchedLocationInLocalStorage(parameters);
@@ -75,15 +75,15 @@ document.querySelector(".input-text-submit").addEventListener("click", ()=>{
             document.querySelector(".current-air-results").setAttribute("style", "display: grid");
             const airQualityResults = new AirQuality(resolve.data);
             airQualityResults.showAirQuality();
-            console.log(geoCords);       
+            console.log(geoCords);
             airQualityResults.saveGeoCordsFromLastSearchedLocation(geoCords);
-            localStorage.setItem("airQualityFlag", "false");   
+            localStorage.setItem("airQualityFlag", "false");
         }).catch(error => {
             Error(error);
             document.querySelector(".current-air-results").setAttribute("style", "display: none");
-            localStorage.setItem("airQualityFlag", "true");             
+            localStorage.setItem("airQualityFlag", "true");
         })
     })
-    .catch((error => Error(error)))
+        .catch((error => Error(error)))
 });
 
