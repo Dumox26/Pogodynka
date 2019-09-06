b3849fc4ea5cc812d87ab5f160a02198cdb6c421
Sebastian <Dumox@someplace.com>
2019-09-06 21:08:31 +0200

    Delete old code

--- js/main.js
+++ js/main.js
@@ -34,73 +34,29 @@ function loadAirQualityData(lat="52.23", lng="21.01"){
     })
 }
 
-// Gelokalizacja
+// Geolocalization
 navigator.geolocation.getCurrentPosition((position) => {
     const parameters = {
         lat: position.coords.latitude,
         lon: position.coords.longitude
     }
-
-    // WeatherClass.downloadCurrentWeatherConditions(parameters).then(resolve => {
-    //     const currentWeaherConditions = new WeatherClass(resolve.data);
-    //     currentWeaherConditions.showCurrentWeatherConditions();
-    //     AirQuality.downloadAirQualityData(position.coords.latitude, position.coords.longitude).then(resolve => {
-
-    //         const airQualityResults = new AirQuality(resolve.data);
-    //         document.querySelector(".current-air-results").setAttribute("style", "display: grid");
-    //         airQualityResults.showAirQuality();
-
-    //     }).catch(error => { Error(error) })
-    // })
-    //     .catch((error => Error(error)))
-
     loadWeatherData(parameters);
 
 }, reject => {
-    //Nie udzielenie zgody na geolokaizacje
-    //wywoływanie pogody dla ostatniej lokalizacji
+    //refuse to geolocalization
+    //download data for last searched localization
+
     if (localStorage.getItem("weatherConditionsLastLocation") != null) {
         const lastSearchedLocalization = JSON.parse(localStorage.getItem("weatherConditionsLastLocation"));
-
-        // WeatherClass.downloadCurrentWeatherConditions(lastSearchedLocalization).then(resolve => {
-        //     const currentWeatherConditions = new WeatherClass(resolve.data);
-        //     currentWeatherConditions.showCurrentWeatherConditions();
-        //     const lastSearchedLocalizationGeoCords = JSON.parse(localStorage.getItem("airQualityGeoCords"));
-
-        //     if (localStorage.getItem("airQualityFlag") == "false") {
-        //         document.querySelector(".current-air-results").setAttribute("style", "display: grid");
-        //         AirQuality.downloadAirQualityData(lastSearchedLocalizationGeoCords.lat, lastSearchedLocalizationGeoCords.lng).then(resolve => {
-        //             const airQualityResults = new AirQuality(resolve.data)
-        //             airQualityResults.showAirQuality();
-        //         })
-
-        //     } else {
-        //         document.querySelector(".current-air-results").setAttribute("style", "display: none");
-        //     }
-        // })
         loadWeatherData(lastSearchedLocalization);
 
     } else {
-        // console.log("LocaL storage jest pusty");
-        // WeatherClass.downloadCurrentWeatherConditions().then(resolve => {
-        //     const currentWeatherClass = new WeatherClass(resolve.data);
-        //     console.log(resolve.data);
-        //     currentWeatherClass.showCurrentWeatherConditions();
-
-        //     AirQuality.downloadAirQualityData().then(resolve => {
-        //         document.querySelector(".current-air-results").setAttribute("style", "display: grid");
-        //         const airQualityResults = new AirQuality(resolve.data);
-        //         airQualityResults.showAirQuality();
-        //     })
-        // }).catch(error =>{
-        //     Error(error);
-        // })
         loadWeatherData();
     }
 
 });
 
-//Obsługa wyszukiwania przez submit
+// submit click
 
 document.querySelector(".input-text-submit").addEventListener("click", ()=>{
     event.preventDefault()
@@ -113,36 +69,3 @@ document.querySelector(".input-text-submit").addEventListener("click", ()=>{
     }
     document.querySelector(".input-text").value = ""; 
 })
-
-// document.querySelector(".input-text-submit").addEventListener("click", () => {
-//     event.preventDefault();
-//     const city = {
-//         q: document.querySelector(".input-text").value
-//     }
-//     document.querySelector(".input-text").value = "";
-
-//     WeatherClass.downloadCurrentWeatherConditions(city).then(resolve => {
-//         const currentWeaherConditions = new WeatherClass(resolve.data);
-//         currentWeaherConditions.showCurrentWeatherConditions();
-//         currentWeaherConditions.savelastSearchedLocationInLocalStorage(city);
-//         const geoCords = {
-//             lat: resolve.data.coord.lat,
-//             lng: resolve.data.coord.lon
-//         }
-
-//         AirQuality.downloadAirQualityData(geoCords.lat, geoCords.lng).then(resolve => {
-//             document.querySelector(".current-air-results").setAttribute("style", "display: grid");
-//             const airQualityResults = new AirQuality(resolve.data);
-//             airQualityResults.showAirQuality();
-//             airQualityResults.saveGeoCordsFromLastSearchedLocation(geoCords);
-//             localStorage.setItem("airQualityFlag", "flag");
-
-//         }).catch(error => {
-//             Error(error);
-//             document.querySelector(".current-air-results").setAttribute("style", "display: none");
-//             //localStorage.setItem("airQualityFlag", "true");
-//         })
-//     })
-//         .catch((error => Error(error)))
-// });
-
