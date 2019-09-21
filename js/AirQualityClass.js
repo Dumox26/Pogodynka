14599714237ae9e8214d827d4eb5716ae314f5ce
Sebastian <Dumox@someplace.com>
2019-09-21 17:42:09 +0200

    Add eslint and styleint to project
    change formatting acording to eslint and styleint regules
    
    2 spaces intead of tab(4 spaces), spaces where they are required,
    emptylines itp

--- js/AirQualityClass.js
+++ js/AirQualityClass.js
@@ -1,50 +1,52 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-undef */
 class AirQuality {
-    constructor(apiResult){
-        this.description = apiResult.current.indexes[0].description;
-        this.caqi = Math.round(apiResult.current.indexes[0].value);
-        this.advice = apiResult.current.indexes[0].advice;
-        this.pm25 = Math.round(apiResult.current.standards[0].percent);
-        this.pm10 = Math.round(apiResult.current.standards[1].percent);
-        this.caqiColor = apiResult.current.indexes[0].color;
-    };
+  constructor(apiResult) {
+    this.description = apiResult.current.indexes[0].description;
+    this.caqi = Math.round(apiResult.current.indexes[0].value);
+    this.advice = apiResult.current.indexes[0].advice;
+    this.pm25 = Math.round(apiResult.current.standards[0].percent);
+    this.pm10 = Math.round(apiResult.current.standards[1].percent);
+    this.caqiColor = apiResult.current.indexes[0].color;
+  }
 
-    saveGeoCordsFromLastSearchedLocation(airQualityData){
-        try{
-            localStorage.setItem("airQualityGeoCords", JSON.stringify(airQualityData));
-        }catch(error){
-            Error(error);
-        }
-    };
+  saveGeoCordsFromLastSearchedLocation(airQualityData) {
+    try {
+      localStorage.setItem('airQualityGeoCords', JSON.stringify(airQualityData));
+    } catch (error) {
+      Error(error);
+    }
+  }
 
-    showAirQuality(){
-        const airQualityDescription = document.querySelector(".air-describe-value");
-        const airQualityCaqiValue = document.querySelector(".caqi-value");
-        const airQualityAdvice =  document.querySelector(".air-quality-description-tooltip");
-        const airQualityPm25 = document.querySelector(".pm25-value");
-        const airQualityPm10 = document.querySelector(".pm10-value");
-        const airQualityCaqi = document.querySelector(".caqi-cnt");
-        
-        airQualityDescription.textContent = this.description;
-        airQualityCaqiValue.textContent = this.caqi;
-        airQualityAdvice.textContent = this.advice;
-        airQualityPm25.textContent = this.pm25 + "% normy";
-        airQualityPm10.textContent = this.pm10 + "% normy";
-        airQualityCaqi.setAttribute("style", "background: " + this.caqiColor);
-    };
-    
-    static async downloadAirQualityData(lat, lng){
-        console.log(`lat=${lat} lng=${lng}`);
-        const apiURL = "https://airapi.airly.eu/v2/measurements/nearest?indexType=AIRLY_CAQI&maxDistanceKM=10"
-        return await axios({method:"get", url: apiURL, params:{
-            "lat": lat,
-            "lng": lng
-        },
-            headers:{
-                'Accept' : 'application/json',
-                'Accept-Language': 'pl',
-                'apikey': 'VaBZUc4Y74pgY9TL817Dghw2YflPWrxk' 
-            }});
-    }; 
+  showAirQuality() {
+    const airQualityDescription = document.querySelector('.air-describe-value');
+    const airQualityCaqiValue = document.querySelector('.caqi-value');
+    const airQualityAdvice = document.querySelector('.air-quality-description-tooltip');
+    const airQualityPm25 = document.querySelector('.pm25-value');
+    const airQualityPm10 = document.querySelector('.pm10-value');
+    const airQualityCaqi = document.querySelector('.caqi-cnt');
+
+    airQualityDescription.textContent = this.description;
+    airQualityCaqiValue.textContent = this.caqi;
+    airQualityAdvice.textContent = this.advice;
+    airQualityPm25.textContent = `${this.pm25}% normy`;
+    airQualityPm10.textContent = `${this.pm10}% normy`;
+    airQualityCaqi.setAttribute('style', `background: ${this.caqiColor}`);
+  }
+
+  static async downloadAirQualityData(lat, lng) {
+    const apiURL = 'https://airapi.airly.eu/v2/measurements/nearest?indexType=AIRLY_CAQI&maxDistanceKM=10';
+    return axios({
+      method: 'get',
+      url: apiURL,
+      params: { lat, lng },
+      headers: {
+        Accept: 'application/json',
+        'Accept-Language': 'pl',
+        apikey: 'VaBZUc4Y74pgY9TL817Dghw2YflPWrxk',
+      },
+    });
+  }
 }
 
-export default AirQuality
+export default AirQuality;
--- js/main.js
+++ js/main.js
@@ -1,130 +1,130 @@
-import WeatherClass from "./WeatherClass.js"
-import AirQuality from "./AirQualityClass.js"
+/* eslint-disable import/extensions */
+/* eslint-disable no-restricted-globals */
 
-function loadWeatherData(inputValue = { q: "warszawa" }){
-    console.log(inputValue);
-    WeatherClass.downloadCurrentWeatherConditions(inputValue).then(weatherData => {        
-        const currentWeather = new WeatherClass(weatherData.data);
-        currentWeather.showCurrentWeatherConditions();
-        currentWeather.savelastSearchedLocationInLocalStorage(inputValue);
-        loadAirQualityData(weatherData.data.coord.lat, weatherData.data.coord.lon);
-    })
-    
-    .catch(error => {
-        Error(error);
-        console.log(error);
-        showErrorMessage(inputValue.q);
-    })
+import WeatherClass from './WeatherClass.js';
+import AirQuality from './AirQualityClass.js';
+
+function loadAirQualityData(lat = '52.23', lng = '21.01') {
+  const currentAirResults = document.querySelector('.current-air-results');
+  const loaderCnt = document.querySelector('.loader-cnt');
+
+  AirQuality.downloadAirQualityData(lat, lng).then((airQualityData) => {
+    const currentAirQuality = new AirQuality(airQualityData.data);
+    currentAirResults.setAttribute('style', 'display: grid');
+    currentAirQuality.showAirQuality();
+    const geoCords = {
+      lat,
+      lng,
+    };
+    currentAirQuality.saveGeoCordsFromLastSearchedLocation(geoCords);
+    localStorage.setItem('airQualityFlag', 'flag');
+    loaderCnt.setAttribute('style', 'display: none');
+  })
+
+    .catch((error) => {
+      Error(error);
+      currentAirResults.setAttribute('style', 'display: none');
+      localStorage.removeItem('airQualityFlag');
+      loaderCnt.setAttribute('style', 'display: none');
+    });
 }
 
-function loadAirQualityData(lat="52.23", lng="21.01"){
-    const currentAirResults = document.querySelector(".current-air-results");
-    AirQuality.downloadAirQualityData(lat, lng).then(airQualityData => {
-        const currentAirQuality = new AirQuality(airQualityData.data);      
-        currentAirResults.setAttribute("style", "display: grid")
-        currentAirQuality.showAirQuality();
-        const geoCords = {
-            lat: lat,
-            lng: lng
-        }
-        currentAirQuality.saveGeoCordsFromLastSearchedLocation(geoCords);
-        localStorage.setItem("airQualityFlag", "flag");
-        document.querySelector(".loader-cnt").setAttribute("style", "display: none");
-    })
+function resizeContainer(container) {
+  const containerToResize = document.querySelector(`.${container}`);
+  const containerToResizeStyle = window.getComputedStyle(containerToResize);
 
-    .catch(error => {
-        Error(error);
-        currentAirResults.setAttribute("style", "display: none");
-        localStorage.removeItem("airQualityFlag");
-        document.querySelector(".loader-cnt").setAttribute("style", "display: none");
-    })
+  if (containerToResizeStyle.getPropertyValue('display') === 'flex') {
+    const currentResultsCnt = document.querySelector('.current-results-cnt');
+    const currentResultsHeader = document.querySelector('.current-results-header');
+    const containerSize = currentResultsCnt.clientHeight - currentResultsHeader.clientHeight;
+    containerToResize.style.height = `${containerSize}px`;
+  }
 }
 
-function bindSearchButton (){
-    event.preventDefault();
+function showErrorMessage(inputValue) {
+  const loaderCnt = document.querySelector('.loader-cnt');
+  loaderCnt.setAttribute('style', 'display: none');
 
-    const errorMessageCnt = document.querySelector(".error-message-cnt");
-    errorMessageCnt.setAttribute("style", "display: none");
+  const errorMessageCnt = document.querySelector('.error-message-cnt');
+  errorMessageCnt.setAttribute('style', 'display: flex');
 
-    const loaderCnt = document.querySelector(".loader-cnt");
-    loaderCnt.setAttribute("style", "display: flex");
+  resizeContainer('error-message-cnt');
 
-    const inputText = document.querySelector(".input-text");
-    resizeContainer("loader-cnt");
-    
-    const city = {
-            q: inputText.value
-        };   
+  const currentCity = document.querySelector('.current-city');
+  currentCity.textContent = 'Błąd';
 
-    if(inputText.value != ""){
-        loadWeatherData(city);
-    }
-
-    inputText.blur();
-    inputText.value = ""; 
+  const errorMessage = errorMessageCnt.querySelector('.error-message');
+  errorMessage.textContent = `Nie znalezionio danych dla ${inputValue}. \n
+                                    Upewnij się, że wpisałeś poprawne dane.`;
 }
 
-function showErrorMessage(inputValue){
-        
-    const loaderCnt = document.querySelector(".loader-cnt");
-    loaderCnt.setAttribute("style", "display: none");
+function loadWeatherData(inputValue = { q: 'warszawa' }) {
+  WeatherClass.downloadCurrentWeatherConditions(inputValue).then((weatherData) => {
+    const currentWeather = new WeatherClass(weatherData.data);
+    currentWeather.showCurrentWeatherConditions();
+    currentWeather.saveLastSearchedLocationInLocalStorage(inputValue);
+    loadAirQualityData(weatherData.data.coord.lat, weatherData.data.coord.lon);
+  })
 
-    const errorMessageCnt = document.querySelector(".error-message-cnt");
-    errorMessageCnt.setAttribute("style", "display: flex");
-
-    resizeContainer("error-message-cnt");
-
-    const currentCity = document.querySelector(".current-city");
-    currentCity.textContent = "Błąd";
-
-    const errorMessage = errorMessageCnt.querySelector(".error-message");
-    errorMessage.textContent = `Nie znalezionio danych dla ${inputValue}. \n
-                                    Upewnij się, że wpisałeś poprawne dane.`;  
+    .catch((error) => {
+      Error(error);
+      showErrorMessage(inputValue.q);
+    });
 }
 
-function resizeContainer(container){
-    const containerToResize = document.querySelector(`.${container}`);
-    const containerToResizeStyle  = window.getComputedStyle(containerToResize);
+function bindSearchButton() {
+  event.preventDefault();
 
-    if(containerToResizeStyle.getPropertyValue("display") === "flex"){
-        const currentResultsCnt = document.querySelector(".current-results-cnt");
-        const currentResultsHeader = document.querySelector(".current-results-header");
-        const containerSize = currentResultsCnt.clientHeight - currentResultsHeader.clientHeight;
-        containerToResize.style.height = containerSize + "px";
-    }
+  const errorMessageCnt = document.querySelector('.error-message-cnt');
+  errorMessageCnt.setAttribute('style', 'display: none');
+
+  const loaderCnt = document.querySelector('.loader-cnt');
+  loaderCnt.setAttribute('style', 'display: flex');
+
+  const inputText = document.querySelector('.input-text');
+  resizeContainer('loader-cnt');
+
+  const city = {
+    q: inputText.value,
+  };
+
+  if (inputText.value !== '') {
+    loadWeatherData(city);
+  }
+
+  inputText.blur();
+  inputText.value = '';
 }
 
- //Resize loader
+// Resize loader
 
-resizeContainer("loader-cnt");
-window.addEventListener("resize", function(){
-    resizeContainer("loader-cnt");
-    resizeContainer("error-message-cnt");
+resizeContainer('loader-cnt');
+window.addEventListener('resize', () => {
+  resizeContainer('loader-cnt');
+  resizeContainer('error-message-cnt');
 });
 
 // Geolocalization
 navigator.geolocation.getCurrentPosition((position) => {
-    const parameters = {
-        lat: position.coords.latitude,
-        lon: position.coords.longitude
-    }
-    loadWeatherData(parameters);
+  const parameters = {
+    lat: position.coords.latitude,
+    lon: position.coords.longitude,
+  };
+  loadWeatherData(parameters);
+}, () => {
+  // refuse to geolocalization
+  // download data for last searched localization
 
-}, reject => {
-    //refuse to geolocalization
-    //download data for last searched localization
-
-    if (localStorage.getItem("weatherConditionsLastLocation") != null) {
-        const lastSearchedLocalization = JSON.parse(localStorage.getItem("weatherConditionsLastLocation"));
-        loadWeatherData(lastSearchedLocalization);
-
-    } else {
-        loadWeatherData();
-    }
+  if (localStorage.getItem('weatherConditionsLastLocation') != null) {
+    const lastSearchedLocalization = JSON.parse(localStorage.getItem('weatherConditionsLastLocation'));
+    loadWeatherData(lastSearchedLocalization);
+  } else {
+    loadWeatherData();
+  }
 });
 
 // submit click
 
-document.querySelector(".input-text-submit").addEventListener("click", bindSearchButton, false);
-document.querySelector(".input-text-submit").addEventListener("touch", bindSearchButton, false);
-document.querySelector(".input-text-submit").addEventListener("submit", bindSearchButton, false);
+document.querySelector('.input-text-submit').addEventListener('click', bindSearchButton, false);
+document.querySelector('.input-text-submit').addEventListener('touch', bindSearchButton, false);
+document.querySelector('.input-text-submit').addEventListener('submit', bindSearchButton, false);
